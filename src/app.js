269f83f638a7d7bd78c931a01e4abb549eb6eab8
Viktor Kovalev <vkkovalev91@gmail.com>
2019-10-12 20:47:52 +0300

    init commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import './index.css';
+import helper from './helper/helper'
+
+function a() {
+  return helper(1,2);
+}
+console.log(a())
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use:  [
+          {
+            loader: 'html-loader',
+            options: { minimize: false },
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ]
+  },
+  plugins: [
+    new  HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  devtool: 'inline-source-map',
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+  },
+};
